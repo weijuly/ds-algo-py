4678658c485076adbf8bf10bc32daacb11ee9359
GopiKrishnan Ganesan <gopikrishnan.ganesan@gmail.com>
2018-11-18 20:24:53 +0530

    remove unwanted lines

--- data_structures/heaps/max_heap.py
+++ data_structures/heaps/max_heap.py
@@ -22,6 +22,7 @@ def adjustUp(heap):
         e_idx, p_idx = p_idx, p_idx // 2
     return heap
 
+
 def adjustDn(heap):
     if not heap or len(heap) < 2:
         return heap
@@ -33,6 +34,7 @@ def adjustDn(heap):
             heap[e_idx], heap[p_idx] = heap[p_idx], heap[e_idx]
         p_idx, e_idx = e_idx, e_idx * 2
     return heap
+
 
 def insert(heap, data):
     if not heap:
@@ -47,39 +49,3 @@ def remove(heap):
     elem = heap.pop(1)
     heap.insert(1, heap.pop())
     return elem, adjustDn(heap)
-
-
-heap = None
-heap = insert(heap, 5)
-heap = insert(heap, 9)
-heap = insert(heap, 11)
-heap = insert(heap, 14)
-heap = insert(heap, 18)
-heap = insert(heap, 19)
-heap = insert(heap, 21)
-heap = insert(heap, 33)
-heap = insert(heap, 17)
-heap = insert(heap, 27)
-print(heap)
-elem, heap = remove(heap)
-print(elem, heap)
-elem, heap = remove(heap)
-print(elem, heap)
-elem, heap = remove(heap)
-print(elem, heap)
-elem, heap = remove(heap)
-print(elem, heap)
-elem, heap = remove(heap)
-print(elem, heap)
-elem, heap = remove(heap)
-print(elem, heap)
-elem, heap = remove(heap)
-print(elem, heap)
-elem, heap = remove(heap)
-print(elem, heap)
-elem, heap = remove(heap)
-print(elem, heap)
-elem, heap = remove(heap)
-print(elem, heap)
-elem, heap = remove(heap)
-print(elem, heap)
